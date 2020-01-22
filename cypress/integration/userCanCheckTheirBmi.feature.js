40c3241e369b3dd23b1e2929f13d15e70bffdf3e
Hunter V <hmvitous@yahoo.com>
2020-01-22 19:22:20 +0100

    Adding calculate button and two inputs for weight and height

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -2,6 +2,9 @@ describe('User can check their BMI',() => {
     it('user can enter weight and height', () => {
         cy.visit('http://localhost:3001')
         cy.contains('BMI Calculator')
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#calculate').click()
     
     })
     
