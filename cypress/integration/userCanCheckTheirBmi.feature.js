40926a15854d440dc9a72bcc8f9b4ea9afecf233
Hunter V <hmvitous@yahoo.com>
2020-01-22 19:10:09 +0100

    Adding html content

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,6 +1,7 @@
 describe('User can check their BMI',() => {
     it('user can enter weight and height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
     
     })
     
